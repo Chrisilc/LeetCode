18cb88e6b5e1689fe13723ae4da2ce615541a918
chrisilc <chrisilc54@gmail.com>
2014-12-03 22:02:34 +0800

    Create

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+For example,
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+
+Note:
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+For the purpose of this problem, we define empty string as valid palindrome.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+    }
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
