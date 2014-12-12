23aff941b2d47c6d1d77b4a8f27bf17e108a154d
chrisilc <chrisilc54@gmail.com>
2014-12-12 23:58:16 +0800

    d

---
+++ MinStack/main.cpp
@@ -0,0 +1,36 @@
+/*
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class MinStack {
+public:
+    void push(int x) {
+
+    }
+
+    void pop() {
+
+    }
+
+    int top() {
+
+    }
+
+    int getMin() {
+
+    }
+};
+
+int main()
+{
+    return 0;
+}
